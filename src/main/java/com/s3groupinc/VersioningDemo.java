430edee3879210cf839a97b03604e4409835fde6
bdinesh <bdinesh@s3groupinc.com>
2019-11-29 21:40:48 +0530

    testing

---
+++ src/main/java/com/s3groupinc/VersioningDemo.java
@@ -0,0 +1,13 @@
+package com.s3groupinc;
+
+/**
+ * Hello world!
+ *
+ */
+public class VersioningDemo
+{
+    public static void main( String[] args )
+    {
+        System.out.println( VersioningDemo.class.getPackage().getImplementationVersion() );
+    }
+}
